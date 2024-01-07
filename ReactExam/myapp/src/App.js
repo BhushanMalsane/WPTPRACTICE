dc5ba2b909c7eca87f1c4dedeb8045f0ba371c5c
BhushanMalsane <malsanebhushan@gmail.com>
2024-01-08 00:01:55 +0530

    update

--- ReactExam/myapp/src/App.js
+++ ReactExam/myapp/src/App.js
@@ -1,23 +1,13 @@
 import logo from './logo.svg';
 import './App.css';
+import ICard from './Icard';
 
 function App() {
+
+  let obj = {name:"priti",dob:"1-1-2000",phone:"1233455"} 
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <ICard obj={obj}></ICard>
     </div>
   );
 }
